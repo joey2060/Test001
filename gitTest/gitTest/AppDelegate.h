ad9d0dacec529ac5668d596bbf26a0783acf0282
joey <jinxiao007@163.com>
2015-06-08 17:56:02 +0800

    init
    init project

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by Product on 15/6/8.
+//  Copyright (c) 2015年 Product. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by Product on 15/6/8.
+//  Copyright (c) 2015年 Product. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
